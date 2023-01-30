988a888d6cba6a0733277ffa2853732a129f6429
Yashchenko Victor <Andrey@Komp11.local>
2023-01-30 14:34:24 +0300

    trying

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,28 +1,54 @@
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { AppModule } from './../src/app.module';
+import { AppService } from '../src/app.service'
 import { INestApplication } from '@nestjs/common';
 
 
-const mockCallback = jest.fn(() => {
-  return 'fofofo';
-})
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let bookService = { getAllBooks: () => ['test'], create: f => f, delete: id => id };
 
   beforeAll(async () => {
     const moduleFixture = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+    .overrideProvider(AppService)
+    .useValue(bookService)
+    .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/GET books', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/books')
       .expect(200)
-      .expect('Hello World!');
+      .expect(
+        bookService.getAllBooks(),
+      );
   });
+
+  it('/POST book', (f) => {
+    return request(app.getHttpServer())
+      .get('/books')
+      .expect(200)
+      .expect(
+        bookService.create(f),
+      );
+  });
+
+  it('/DELETE book', (id) => {
+    return request(app.getHttpServer())
+      .get('/books')
+      .expect(200)
+      .expect(
+        bookService.delete(id),
+      );
+  });
+  afterAll(async () => {
+    await app.close();
+  });
+
 });
